0278ced88f3c1cb2f79edcec8d83165ddce9794f
TrickWithATwist <djomehr2@msu.edu>
2025-04-11 17:47:59 -0400

    made css file for context

--- duoview/src/app/context/page.tsx
+++ duoview/src/app/context/page.tsx
@@ -1,9 +1,15 @@
 'use client';
+import {useState} from 'react';
+import styles from './context.module.css'
 
 export default function context()
 {
     return(
-        <div>
+        <div className='contextpage'>
+            <h1>Submit Context For Interview</h1>
+            <div className='context-form'>
+
+            </div>
             <p>Context page</p>
         </div>
     )
