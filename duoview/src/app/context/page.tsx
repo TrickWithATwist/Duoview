bedece715bcb40afd7983894c52cbe5d3342e028
TrickWithATwist <djomehr2@msu.edu>
2025-04-11 18:09:58 -0400

    WIP commit context page

--- duoview/src/app/context/page.tsx
+++ duoview/src/app/context/page.tsx
@@ -5,9 +5,9 @@ import styles from './context.module.css'
 export default function context()
 {
     return(
-        <div className='contextpage'>
-            <h1>Submit Context For Interview</h1>
-            <div className='context-form'>
+        <div className={styles.contextform}>
+            <h1 className={styles.contextheader}>Submit context for your interview!</h1>
+            <div>
 
             </div>
             <p>Context page</p>
