c2243db30b8821a6954b69f4ec7ba78df080fffc
TrickWithATwist <djomehr2@msu.edu>
2025-04-11 21:01:43 -0400

    current build

--- duoview/src/app/context/page.tsx
+++ duoview/src/app/context/page.tsx
@@ -6,16 +6,27 @@ export default function context()
 {
     return(
         <div className={styles.contextform}>
-            <h1 className={styles.contextheader} htmlFor='resumeUpload'>Submit context for your interview!</h1>
+            <h1 className={styles.contextheader}>Submit context for your interview!</h1>
+
+            <p>Copy and paste company info and job description here</p>
             <div>
-                <label className={styles.resumefile}>
+                <textarea className={styles.textinput}>
+
+                </textarea>
+            </div>
+
+
+            <p>Upload PDF of your resume</p>
+            <div>
+                <label className={styles.resumefile} htmlFor='resumeUpload'>
+                Upload Resume
+                </label>
                 <input
                     type="file"
                     id="resumeUpload"
                     accept=".pdf"
                     className={styles.fileInput}
                 />
-                </label>
             </div>
             <p>Context page</p>
         </div>
