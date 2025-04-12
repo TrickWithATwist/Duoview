000f6b2b00f68c05d334f77f6eb3a3a17bd8b0af
TrickWithATwist <djomehr2@msu.edu>
2025-04-12 07:12:40 -0400

    Starting page complete

--- duoview/src/app/page.tsx
+++ duoview/src/app/page.tsx
@@ -1,7 +1,27 @@
+'use client'
 import Image from "next/image";
+import "./globals.css";
+import styles from './home.module.css';
+import { usePathname } from "next/navigation";
+import { useRouter } from 'next/navigation';
+
 
 export default function Home() {
+  const router = useRouter()
+
+  function HandleClick()
+  {
+    router.push("/context")
+  }
+
   return (
-   <p>Home</p>
+   <div className={styles.homepage}>
+    <div className={styles.duoviewlogo}>
+    </div>
+    <p className={styles.duoviewp}>Practice behavioral interviews with ease</p>
+    <button className={styles.startbutton} onClick={HandleClick}>
+      <span className={styles.text}>Click to start</span>
+    </button>
+   </div>
   );
 }
